3e732ee40a291da858804219caa9b9cf8574d98d
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-03-20 04:05:55 +0530

    Take this code

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+	if(num === 1)return 0;
+	if(num === 2 )return 1;
+	let a = 0;
+	let b = 1;
+	let sum = 0;
+	for(let i = 2; i< num; i++){
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	return sum;
 }
 
 module.exports = fibonacci;
